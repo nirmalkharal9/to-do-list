891e3203aedb5ed2018a881839cde4d5481ddb2b
QCS <QCS@DESKTOP-SCM1O7S>
2023-12-10 20:38:32 +0545

    Add project files.

---
+++ to do list/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Column1 = New System.Windows.Forms.DataGridViewButtonColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.ForestGreen
+        Me.Button1.Location = New System.Drawing.Point(431, 204)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "add "
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(145, 57)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(279, 37)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(145, 204)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(280, 43)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(69, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Padding = New System.Windows.Forms.Padding(10)
+        Me.Label1.Size = New System.Drawing.Size(53, 36)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(71, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Date"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1})
+        Me.DataGridView1.Location = New System.Drawing.Point(72, 277)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(635, 180)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Button2.Location = New System.Drawing.Point(586, 204)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 43)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "load"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(145, 124)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(279, 22)
+        Me.DateTimePicker1.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(69, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Padding = New System.Windows.Forms.Padding(10)
+        Me.Label3.Size = New System.Drawing.Size(38, 36)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Id"
+        '
+        'Column1
+        '
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.Red
+        Me.Column1.DefaultCellStyle = DataGridViewCellStyle1
+        Me.Column1.HeaderText = "delete"
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.Text = "Delete"
+        Me.Column1.ToolTipText = "Delete"
+        Me.Column1.UseColumnTextForButtonValue = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "TO DO"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Column1 As DataGridViewButtonColumn
+End Class
---
+++ to do list/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Diagnostics.Eventing
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim mysqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        mysqlConn = New MySqlConnection
+        mysqlConn.ConnectionString = "server=localhost;userid=root;password='';database=todo"
+
+        Dim READER As MySqlDataReader
+        Dim title As String = TextBox1.Text
+        Dim id As String = TextBox2.Text
+
+        Dim datex As DateTime = DateTimePicker1.Value
+        If title.Length < 1 Then
+            MessageBox.Show("title is empty, please enter title.")
+        ElseIf id.Length < 1 Then
+            MessageBox.Show("title is empty, please enter your id.")
+        Else
+
+            Try
+                mysqlConn.Open()
+                Dim query As String
+                query = "insert into todo.list(id ,title,date) values('" & TextBox2.Text & " ','" & TextBox1.Text & " ','" & datex & " '
+)"
+                COMMAND = New MySqlCommand(query, mysqlConn)
+                READER = COMMAND.ExecuteReader
+                MessageBox.Show("Data Saved")
+
+                mysqlConn.Close()
+                mysqlConn.Dispose()
+            Catch ex As MySqlException
+                MessageBox.Show(ex.Message)
+                mysqlConn.Close()
+            Finally
+                mysqlConn.Dispose()
+            End Try
+        End If
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        mysqlConn = New MySqlConnection
+        mysqlConn.ConnectionString = "server=localhost;userid=root;password='';database=todo"
+
+        Dim READER As MySqlDataReader
+
+        Try
+            mysqlConn.Open()
+            Dim query As String
+            query = "select id , title,date from todo.list where id = '" & TextBox2.Text & " '"
+
+            COMMAND = New MySqlCommand(query, mysqlConn)
+            READER = COMMAND.ExecuteReader
+
+            Dim dataTable As New DataTable()
+            dataTable.Load(READER)
+
+            DataGridView1.DataSource = dataTable
+            READER.Close()
+
+
+
+            mysqlConn.Close()
+            mysqlConn.Dispose()
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+            mysqlConn.Close()
+        Finally
+            mysqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim data As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+        Dim title As String = data.Cells("title").Value.ToString()
+        Dim id As String = data.Cells("id").Value.ToString()
+
+
+        Dim READER As MySqlDataReader
+        Try
+            mysqlConn.Open()
+            Dim query As String
+            query = "delete from todo.list where id = '" & id & " ' and title ='" & title & " ' "
+
+            COMMAND = New MySqlCommand(query, mysqlConn)
+            COMMAND.ExecuteReader()
+
+            MessageBox.Show("work deleted.")
+
+
+
+            mysqlConn.Close()
+            mysqlConn.Dispose()
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+            mysqlConn.Close()
+        Finally
+            mysqlConn.Dispose()
+        End Try
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
